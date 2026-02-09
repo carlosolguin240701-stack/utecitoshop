80c1d217fef2d232ea3bbaae50fc2072a82cd67f
carlosolguin240701-stack <carlosolguin240701@gmail.com>
2026-02-09 17:42:04 -0600

    Update proyectos.js

--- proyectos.js
+++ proyectos.js
@@ -1,66 +1,33 @@
-let proyectos = JSON.parse(localStorage.getItem("proyectos")) || [];
+const proyectos = [
+  {
+    titulo: "Proyecto Academico UTC",
+    imagen: "utc.png"
+  }
+];
 
-const lista = document.getElementById("lista");
-const catalogo = document.getElementById("catalogo");
+const contenedor = document.getElementById("catalogo");
 
-if (lista) mostrarAdmin();
-if (catalogo) mostrarCatalogo();
-
-function mostrarAdmin() {
-  lista.innerHTML = "";
-  proyectos.forEach((p, i) => {
-    lista.innerHTML += `
-      <div class="card">
-        <img src="${p.img}" onclick="verImagen('${p.img}')">
-        <h3>${p.titulo}</h3>
-        <p>${p.desc}</p>
-        <button onclick="eliminar(${i})">Eliminar</button>
-      </div>
-    `;
-  });
-}
-
-function mostrarCatalogo() {
-  catalogo.innerHTML = "";
+if (contenedor) {
   proyectos.forEach(p => {
-    catalogo.innerHTML += `
-      <div class="card">
-        <img src="${p.img}" onclick="verImagen('${p.img}')">
-        <h3>${p.titulo}</h3>
-        <p>${p.desc}</p>
-      </div>
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+      <img src="${p.imagen}" onclick="verImagen('${p.imagen}')">
+      <h3>${p.titulo}</h3>
     `;
+    contenedor.appendChild(card);
   });
 }
 
-const form = document.getElementById("formProyecto");
-if (form) {
-  form.addEventListener("submit", e => {
-    e.preventDefault();
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImg");
 
-    const titulo = document.getElementById("titulo").value;
-    const desc = document.getElementById("descripcion").value;
-    const file = document.getElementById("imagen").files[0];
-
-    const reader = new FileReader();
-    reader.onload = () => {
-      proyectos.push({
-        titulo: titulo,
-        desc: desc,
-        img: reader.result
-      });
-
-      localStorage.setItem("proyectos", JSON.stringify(proyectos));
-      mostrarAdmin();
-      form.reset();
-    };
-
-    reader.readAsDataURL(file);
-  });
+function verImagen(src) {
+  modal.style.display = "flex";
+  modalImg.src = src;
 }
 
-function eliminar(i) {
-  proyectos.splice(i, 1);
-  localStorage.setItem("proyectos", JSON.stringify(proyectos));
-  mostrarAdmin();
-}
+modal?.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
