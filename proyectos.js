dc50f233c5aa8a5877b919cd73066702623a9e1c
carlosolguin240701-stack <carlosolguin240701@gmail.com>
2026-02-09 18:18:53 -0600

    Update proyectos.js

--- proyectos.js
+++ proyectos.js
@@ -1,33 +1,50 @@
-const proyectos = [
-  {
-    titulo: "Proyecto Academico UTC",
-    imagen: "utc.png"
-  }
-];
-
-const contenedor = document.getElementById("catalogo");
-
-if (contenedor) {
-  proyectos.forEach(p => {
-    const card = document.createElement("div");
-    card.className = "card";
-    card.innerHTML = `
-      <img src="${p.imagen}" onclick="verImagen('${p.imagen}')">
-      <h3>${p.titulo}</h3>
-    `;
-    contenedor.appendChild(card);
-  });
-}
-
+const catalogo = document.getElementById("catalogo");
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modalImg");
+const btnAgregar = document.getElementById("btnAgregar");
+
+let proyectos = JSON.parse(localStorage.getItem("proyectos")) || [];
+
+// Ver login
+if (localStorage.getItem("login") !== "true") {
+  btnAgregar.style.display = "none";
+}
+
+function render() {
+  catalogo.innerHTML = "";
+
+  proyectos.forEach((p, i) => {
+    const div = document.createElement("div");
+    div.className = "card";
+
+    div.innerHTML = `
+      <img src="${p.imagen}" onclick="verImagen('${p.imagen}')">
+      <h3>${p.nombre}</h3>
+      ${
+        localStorage.getItem("login") === "true"
+          ? `<button onclick="eliminar(${i})">Eliminar</button>`
+          : ""
+      }
+    `;
+
+    catalogo.appendChild(div);
+  });
+}
 
 function verImagen(src) {
   modal.style.display = "flex";
   modalImg.src = src;
 }
 
-modal?.addEventListener("click", () => {
+modal.addEventListener("click", () => {
   modal.style.display = "none";
 });
+
+function eliminar(index) {
+  proyectos.splice(index, 1);
+  localStorage.setItem("proyectos", JSON.stringify(proyectos));
+  render();
+}
+
+render();
 
